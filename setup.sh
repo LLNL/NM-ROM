70fc07dcc86f81f61fd61c443472b404a7fb32cd
Kevin Huynh <huynh24@llnl.gov>
2020-09-30 13:36:30 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 python3 -m venv nm-rom
 source nm-rom/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 python -m ipykernel install --user --name nm-rom --display-name "NM-ROM"
